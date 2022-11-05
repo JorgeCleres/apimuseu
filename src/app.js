1f9cbb345fbb782481f2bae0b60174a0c62a21e2
JorgeCleres <jrgcleres@hotmail.com>
2022-11-04 23:01:34 -0300

    fast

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ const app = express()
 
 const index = require('./routes/index')
 
+const message = require('./routes/message.routes')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.json({ type: 'application/vnd.api+json'}))
 app.use(cors()) 
 
-app.use('', index)
+app.use('', message)
 
 module.exports = app
