058cd3413c004d86c8712baf7eb97cb1cb4b865b
JorgeCleres <jrgcleres@hotmail.com>
2022-11-05 00:10:12 -0300

    mudandaça texto

--- src/controller/messageController.js
+++ src/controller/messageController.js
@@ -2,18 +2,22 @@ const Message = require('../model/messageModel')
 //const PdfDocument = require('../services/PdfDocument')
 
 exports.registerMessage = async(req, res) => {
-    try {
-        let item = {
-            message: req.body.mensagem
-        }
-        let saveMessage = await new Message(item)
-        saveMessage.save()
-        res.status(201).json({ message: 'Confirm criado com sucesso', saveMessage})
-    }
-    catch (err) {
-        res.status(400).json({message: 'erro', err: err})
-        console.log(err)
-    }
+    // try {
+    //     let item = {
+    //         message: req.body.mensagem
+    //     }
+    //     let saveMessage = await new Message(item)
+    //     saveMessage.save()
+    //     res.status(201).json({ message: 'Confirm criado com sucesso', saveMessage})
+    // }
+    // catch (err) {
+    //     res.status(400).json({message: 'erro', err: err})
+    //     console.log(err)
+    // }
+    res.status(200).send({
+        success: true,
+        message: 'get mensagem getmessage new mesage'
+    })
 }
 
 exports.getMessages = async(req, res) => {
