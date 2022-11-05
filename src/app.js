f6e2ebba8740705191a327d8101734ce4edece48
JorgeCleres <jrgcleres@hotmail.com>
2022-11-04 22:41:35 -0300

    terce

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 
 const index = require('./routes/index')
+const message = require('./routes/message.routes')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -11,5 +12,7 @@ app.use(express.json({ type: 'application/vnd.api+json'}))
 app.use(cors()) 
 
 app.use('', index)
+
+app.use('', message)
 
 module.exports = app
---
+++ src/routes/message.routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+//const messageController = require('../controllers/message.controller')
+
+// router.post('/message', messageController.registerMessage)
+
+// router.post('/sendemail', messageController.emailMessage)
+
+// router.get('/getmessage', messageController.getMessages)
+
+router.get('/api2', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: 'usando routes msg'
+    })
+})
+
+module.exports = router
